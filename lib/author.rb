8eddbeecea031a74bb0a0ca1e73a117c6b35d54f
David Anderson <matandersonlv@hotmail.com>
2019-03-08 21:50:33 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,27 @@
+class Author 
+  attr_accessor :name
+  attr_reader :posts
+  
+  @@post_count = 0
+  
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+  
+  def add_post(post)
+    self.posts << post
+    post.author = self
+    @@post_count += 1
+  end
+  
+  def add_post_by_title(title)
+    title = Post.new(title)
+    add_post(title)
+  end
+  
+  def self.post_count
+    @@post_count
+  end
+  
+end
